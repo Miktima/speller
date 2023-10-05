bfe09633b672e02fef6effc2dc4a71fc1473335b
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2023-10-05 17:40:03 +0300

    get article

--- speller.go
+++ speller.go
@@ -1,54 +1,90 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 
 	"golang.org/x/net/html"
 )
 
-func main() {
-	// s := `<p>Links:</p><ul><li><a href="foo">Foo!!!</a><li><a href="/bar/baz">This is a test</a></ul>`
-	url := `https://ria.ru/20231004/diversant-1900408924.html`
-	resp, err := http.Get(url)
+func getHtmlPage(url, userAgent string) ([]byte, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("Error opening URL: ", url)
-		return
+		fmt.Printf("Cannot create new request  %s, error: %v\n", url, err)
+		return nil, err
 	}
+
+	req.Header.Set("User-Agent", "RIA/autotest")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error with GET request: %v\n", err)
+		return nil, err
+	}
+
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error ReadAll")
+		return nil, err
 	}
-	fmt.Println(string(body))
-	tkn := html.NewTokenizer(strings.NewReader(string(body)))
-	var ftkn func(*html.Tokenizer)
-	ftkn = func(t *html.Tokenizer) {
-		depth := 0
-		for {
-			tt := t.Next()
-			switch tt {
-			case html.ErrorToken:
-				return
-			case html.TextToken:
-				if depth > 0 {
-					fmt.Println(string(t.Text()))
-				}
-			case html.StartTagToken, html.EndTagToken:
-				tn, _ := t.TagName()
-				if len(tn) == 1 && tn[0] == 'a' {
-					if tt == html.StartTagToken {
+	return body, nil
+}
+
+func getArticle(body []byte, tag, keyattr, value string) []string {
+	tkn := html.NewTokenizer(bytes.NewReader(body))
+	depth := 0
+	var article []string
+	block := ""
+	errorCode := false
+
+	for !errorCode {
+		tt := tkn.Next()
+		switch tt {
+		case html.ErrorToken:
+			errorCode = true
+		case html.TextToken:
+			if depth > 0 {
+				block += string(tkn.Text())
+			}
+		case html.StartTagToken, html.EndTagToken:
+			tn, tAttr := tkn.TagName()
+			if string(tn) == tag {
+				if tAttr {
+					key, attr, _ := tkn.TagAttr()
+					if tt == html.StartTagToken && string(key) == keyattr && string(attr) == value {
 						depth++
-					} else {
-						depth--
 					}
+				} else if tt == html.EndTagToken && depth >= 1 {
+					depth--
+					article = append(article, block)
+					block = ""
 				}
 			}
 		}
 	}
-	ftkn(tkn)
-	defer resp.Body.Close()
+
+	return article
+}
+
+func speller(artcile []string)
+
+func main() {
+	// s := `<p>Links:</p><ul><li><a href="foo">Foo!!!</a><li><a href="/bar/baz">This is a test</a></ul>`
+	url := `https://ria.ru/20231005/moldaviya-1900611273.html`
+	userAgent := `RIA/autotest`
+
+	body, err := getHtmlPage(url, userAgent)
+	if err != nil {
+		fmt.Printf("Error getHtmlPage - %v\n", err)
+	}
+	article := getArticle(body, "div", "class", "article__text")
+	for _, value := range article {
+		fmt.Println(value)
+	}
 }
