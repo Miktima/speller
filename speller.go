4e248dc2db89bce7f8c22e0b3c803acc63dae659
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2023-10-17 18:04:56 +0300

    debug speller

--- speller.go
+++ speller.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"io"
@@ -14,9 +15,9 @@ import (
 
 type SpellOptions struct {
 	Article []string
-	lang    string
-	options int
-	format  string
+	Lang    string
+	Options int
+	Format  string
 }
 
 func getHtmlPage(url, userAgent string) ([]byte, error) {
@@ -84,18 +85,19 @@ func getArticle(body []byte, tag, keyattr, value string) []string {
 
 func speller(opt SpellOptions) error {
 	httpposturl := "https://speller.yandex.net/services/spellservice.json/checkTexts"
+	// httpposturl := "https://speller.yandex.net/services/spellservice/checkTexts"
 
 	context, err := json.Marshal(opt)
 	if err != nil {
 		fmt.Printf("Error marshal json context - %v\n", err)
 		return err
 	}
-
+	fmt.Println(string(context))
 	request, err := http.NewRequest("POST", httpposturl, bytes.NewBuffer(context))
 	if err != nil {
 		fmt.Printf("Error NewRequest - %v\n", err)
 	}
-	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	client := &http.Client{}
 	response, err := client.Do(request)
@@ -113,34 +115,88 @@ func speller(opt SpellOptions) error {
 func main() {
 	var opt SpellOptions
 	var url string
+	var urlList string
+	// XML structure of RSS
+	type RiaRss struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel struct {
+			Title     string `xml:"title"`
+			Link      string `xml:"link"`
+			Language  string `xml:"language"`
+			Copyright string `xml:"copyright"`
+			Item      []struct {
+				Title     string `xml:"title"`
+				Link      string `xml:"link"`
+				Guid      string `xml:"guid"`
+				Priority  string `xml:"rian:priority"`
+				Pubdate   string `xml:"pubDate"`
+				Type      string `xml:"rian:type"`
+				Category  string `xml:"category"`
+				Enclosure string `xml:"enclosure"`
+			} `xml:"item"`
+		} `xml:"channel"`
+	}
+
 	userAgent := `RIA/autotest`
 	flag.StringVar(&url, "url", "0", "URL of the article")
-	flag.StringVar(&opt.lang, "lang", "ru", "Language being tested")
-	flag.IntVar(&opt.options, "options", 14, "Speller options")
-	flag.StringVar(&opt.format, "format", "plain", "Format of the text ")
+	flag.StringVar(&urlList, "xml", "0", "XML with list of the articles")
+	flag.StringVar(&opt.Lang, "lang", "ru", "Language being tested")
+	flag.IntVar(&opt.Options, "options", 14, "Speller options")
+	flag.StringVar(&opt.Format, "format", "plain", "Format of the text ")
 
 	flag.Parse()
 
-	if url == "0" {
-		fmt.Println(("URL must be specified"))
+	if url == "0" && urlList == "0" {
+		fmt.Println(("URL or XML must be specified"))
 		return
 	}
 
-	body, err := getHtmlPage(url, userAgent)
-	if err != nil {
-		fmt.Printf("Error getHtmlPage - %v\n", err)
-	}
-	article := getArticle(body, "div", "class", "article__text")
-	articleLen := 0
-	for _, value := range article {
-		fmt.Println(value)
-		articleLen += len(value)
-	}
-	fmt.Println("Total length: ", articleLen)
+	if url != "0" {
+		// body, err := getHtmlPage(url, userAgent)
+		// if err != nil {
+		// 	fmt.Printf("Error getHtmlPage - %v\n", err)
+		// }
+		// article := getArticle(body, "div", "class", "article__text")
+		article := []string{"ЖЕНЕВА, 17 окт - РИА Новости. По меньшей мере 24 объкта Агенства ООН по делам помощи палестинским беженцам и организации работ (БАПОР) были повреждены в результате израильских ударов и бомбардировок по всему сектору Газа с 7 октября, и реальная цифра, вероятно, выше, говорится в опубликованном отчете на сайте организации."}
+		articleLen := 0
+		for _, value := range article {
+			fmt.Println(value)
+			articleLen += len(value)
+		}
+		opt.Article = article
+		error := speller(opt)
+		if error != nil {
+			fmt.Printf("Error speller - %v\n", error)
+		}
+	} else if urlList != "0" {
+		rss := new(RiaRss)
+		body, err := getHtmlPage(urlList, userAgent)
+		if err != nil {
+			fmt.Printf("Error getHtmlPage - %v\n", err)
+		}
+		err1 := xml.Unmarshal([]byte(body), rss)
+		if err != nil {
+			fmt.Printf("error: %v", err1)
+			return
+		}
+		for _, value := range rss.Channel.Item {
+			fmt.Println("========>", value.Link)
+			body, err := getHtmlPage(value.Link, userAgent)
+			if err != nil {
+				fmt.Printf("Error getHtmlPage - %v\n", err)
+			}
+			article := getArticle(body, "div", "class", "article__text")
+			articleLen := 0
+			for _, value := range article {
+				fmt.Println(value)
+				articleLen += len(value)
+			}
+			fmt.Println("Total length: ", articleLen)
 
-	error := speller(opt)
-	if error != nil {
-		fmt.Printf("Error speller - %v\n", error)
+			error := speller(opt)
+			if error != nil {
+				fmt.Printf("Error speller - %v\n", error)
+			}
+		}
 	}
-
 }
